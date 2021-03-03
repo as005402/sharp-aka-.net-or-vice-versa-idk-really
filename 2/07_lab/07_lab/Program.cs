256826d79c60de5a79570e87898d67b5b00f7e02
Alina Grigoruk <70770852+as005402@users.noreply.github.com>
2021-03-03 18:56:47 +0300

    Add files via upload

---
+++ 2/07_lab/07_lab/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+
+namespace _07_lab
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int n, c;
+            int[] a, b;
+
+            StreamReader fileIn = new StreamReader("Inlet.in");
+            StreamWriter fileOut = new StreamWriter("Outlet.out");
+
+            n = Convert.ToInt32(fileIn.ReadLine());
+            a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = Convert.ToInt32(fileIn.ReadLine());
+            fileIn.Close();
+
+            int min = a[0];
+            int max = a[0];
+            foreach (int z in a)
+            {
+                if (z > max) max = z;
+                if (z < min) min = z;
+            }
+
+            string s = Convert.ToString(Math.Abs(max - min));
+            fileOut.Write(s);
+            fileOut.Close();
+        }
+    }
+}
---
+++ 2/07_task/07_task/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.IO;
+
+namespace _07_task
+{
+    class Program
+    {
+
+        static void Main(string[] args)
+        {
+            int n, c;
+            int[] a, b;
+
+            StreamReader fileIn = new StreamReader("Inlet.in");
+            StreamWriter fileOut = new StreamWriter("Outlet.out");
+
+            string[] s = fileIn.ReadLine().Split(' ');
+            n = Convert.ToInt32(s[0]);
+            c = Convert.ToInt32(s[1]);
+            a = new int[n];
+            for (int i = 0; i < n; i++) a[i] = Convert.ToInt32(fileIn.ReadLine());
+            fileIn.Close();
+            
+            b = new int[1];
+            for (int i = 1; i < a.Length; i++)
+                if (c % i == 0) { Array.Resize(ref b, b.Length + 1); b[b.Length - 1] = a[i]; }
+
+            s[0] = "";
+            for (int i = 0; i < b.Length; i++)
+                s[0] += " " + b[i];
+            s[0] = s[0].Trim();
+            fileOut.Write(s[0]);
+            fileOut.Close();
+
+
+        }
+    }
+}
